b2512de532e7b757fb52599258744b69da47d507
Ujval Jitendrakumar Shah <54790368+UjvalShahTraveloka@users.noreply.github.com>
2020-03-30 13:59:50 +0800

    Update HelloGrain.cs

--- HelloWorld/HelloWorld.Grains/HelloGrain.cs
+++ HelloWorld/HelloWorld.Grains/HelloGrain.cs
@@ -1,8 +1,36 @@
-﻿using System;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using HelloWorld.Grains;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
 
-namespace HelloWorld.Grains
+namespace SiloHost
 {
-    public class HelloGrain
+    class Program
     {
+        public static async Task Main(string[] args)
+                 {
+                     var builder = new SiloHostBuilder()
+                         .UseLocalhostClustering()
+                         .Configure<ClusterOptions>(options =>
+                         {
+                             options.ClusterId = "dev";
+                             options.ServiceId = "HelloWorld";
+                         })
+                         .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                         .ConfigureApplicationParts(parts =>
+                             parts.AddApplicationPart(typeof(HelloGrain).Assembly).WithReferences())
+                         .ConfigureLogging(logging => logging.AddConsole());
+         
+                     var host = builder.Build();
+                     await host.StartAsync();
+                     
+                     Console.WriteLine("Press enter to terminate !!");
+                     Console.ReadKey();
+         
+                 }
     }
 }
