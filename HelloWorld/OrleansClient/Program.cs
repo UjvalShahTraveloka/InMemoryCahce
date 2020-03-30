75c62e512a949fe9a1c32c328ba6dffb5bdae7e3
Ujval Jitendrakumar Shah <54790368+UjvalShahTraveloka@users.noreply.github.com>
2020-03-30 14:08:35 +0800

    Update Program.cs

--- HelloWorld/OrleansClient/Program.cs
+++ HelloWorld/OrleansClient/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+using System;
+using System.Threading.Tasks;
+using HelloWorld.Interfaces;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
 
 namespace OrleansClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "HelloWorld";
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .Build();
+
+            await client.Connect();
+            Console.WriteLine("Client successfully connect to silo host");
+  
+            await DoClientWork(client);
+            Console.ReadKey();
+
         }
+
+        private static async Task DoClientWork(IClusterClient client)
+        {
+            // example of calling grains from the initialized client
+            var friend = client.GetGrain<IHello>(0);
+            var response = await friend.SayHello("Good morning, my friend!");
+            Console.WriteLine("\n\n{0}\n\n", response);
+        }
+
     }
 }
