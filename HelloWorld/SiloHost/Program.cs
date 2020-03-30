c342b21ebbf86c1aeedf21bb562a95cb068bd08d
Ujval Jitendrakumar Shah <54790368+UjvalShahTraveloka@users.noreply.github.com>
2020-03-30 14:09:07 +0800

    Update Program.cs

--- HelloWorld/SiloHost/Program.cs
+++ HelloWorld/SiloHost/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using HelloWorld.Grains;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
 
 namespace SiloHost
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        public static async Task Main(string[] args)
+                 {
+                     var builder = new SiloHostBuilder()
+                         .UseLocalhostClustering()
+                         .Configure<ClusterOptions>(options =>
+                         {
+                             options.ClusterId = "dev";
+                             options.ServiceId = "HelloWorld";
+                         })
+                         .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                         .ConfigureApplicationParts(parts =>
+                             parts.AddApplicationPart(typeof(HelloGrain).Assembly).WithReferences())
+                         .ConfigureLogging(logging => logging.AddConsole());
+         
+                     var host = builder.Build();
+                     await host.StartAsync();
+                     
+                     Console.WriteLine("Press enter to terminate !!");
+                     Console.ReadKey();
+         
+                 }
     }
 }
